01ebcc73ed32506e814412094a502fa461cd4144
Elijah Florence <efloren@siue.edu>
2019-05-20 21:31:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
+binding.pry 
 def nyc_pigeon_organizer(data)
-  
+  newArray = {}
+  data.each do |key, value|
+    if key == "color"
+      self.each do |key, value|
+        newArray << {key, nil}
+      end
+    end
+    return(newArray)
 end
